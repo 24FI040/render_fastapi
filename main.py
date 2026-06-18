b980f2ebfedc7d1af815d6aa01b0c65167c91a4d
24FI040 <24fi040@ms.dendai.ac.jp>
2026-06-18 23:07:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from omikuji import get_omikuji
+from homepagemaker import get_homepage
 
 app = FastAPI()
 
 @app.get("/omikuji")
 def omikuji():
     return {"result": get_omikuji()}
+
+@app.get("/index")
+def index():
+    return get_homepage()
