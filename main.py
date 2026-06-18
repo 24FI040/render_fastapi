f2da2689b2c0fd742be7e4b956fbd415c9e1b93e
24FI040 <24fi040@ms.dendai.ac.jp>
2026-06-18 23:16:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 from omikuji import get_omikuji
-from homepagemaker import get_homepage
+from xmaspresent import Present, make_response
 
 app = FastAPI()
 
+#omikuji
 @app.get("/omikuji")
 def omikuji():
     return {"result": get_omikuji()}
 
+#homepage
 @app.get("/index")
 def index():
-    return get_homepage()
+    return """
+    <html>
+        <head><title>Some HTML in here</title></head>
+        <body><h1>Look ma! HTML!</h1></body>
+    </html>
+    """
+
+#xmaspresent
+@app.post("/present")
+async def give_present(data: Present):
+    return make_response(data)
